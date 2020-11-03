368831a20d3cf02310743a8f9da5b06b51d61554
Dirtyworker-k2 <1404293933@qq.com>
2020-11-03 14:01:44 +0800

    fix

--- vacancyMigration/vanMig.py
+++ vacancyMigration/vanMig.py
@@ -4,7 +4,7 @@ version:
 Author: sch
 Date: 2020-10-21 14:12:46
 LastEditors: sch
-LastEditTime: 2020-10-22 18:44:58
+LastEditTime: 2020-11-03 13:43:44
 '''
 import numpy as np
 from ase.visualize import view
@@ -13,23 +13,25 @@ from ase.build import cut
 from .distance_bet_plane import inner_product, distance_bet_sites, vector_minus, epsilon_criterion
 import random
 epsilon = 0.3  # delta z < epsilon： sites处于同层
-number_per_layer = 100  # 每层最多30个原子
+number_per_layer = 100  # 每层最多100个原子
 
 
 class vacancyMigration1(object):
     def __init__(self, cifFile=' '):
         # 类传参，可无
         self.cifFile = cifFile
-
         self.crystal = read(cifFile)
 
         # 得到所有阵点之间的距离
         self.distances_bet_sites = self.crystal.get_all_distances()
 
-        # 得到所有阵点的坐标(index --> atoms的编号), 并转化成list形式
+        # 得到所有阵点的坐标t(index --> atoms的编号), 并转化成list形式
         # 95个sites
-        self.site_positions = list(list(x)
+        self.sites_position = list(list(x)
                                    for x in self.crystal.get_positions())
+        # 由于储存每次空位扩散的最终的空位位置
+        self.centers_lst = []
+
 
 
 # 一、 原子分层
@@ -37,8 +39,7 @@ class vacancyMigration1(object):
     def get_layers(self):
         # (1) 取出所有阵点的z(unrepeated)，z的数目 代表 层数
         self.z_lst = [0]
-        mark = 1
-        for i in self.site_positions:
+        for i in self.sites_position:
             mark = 1
             for j in self.z_lst:
                 if (epsilon_criterion(x=i[2], standard=j, epsilon=epsilon)):
@@ -52,8 +53,8 @@ class vacancyMigration1(object):
 
         # (2) 将不同层(z不同)原子分组
         # note: [[]] * num : 一种极其傻逼的定义多维数组的方式
-        self.siteLabel_per_layer = [[] for i in range(len(self.z_lst))]
-        for i in self.site_positions:
+        self.sitesLabel_per_layer = [[] for i in range(len(self.z_lst))]
+        for i in self.sites_position:
             for j in self.z_lst:
                 if (epsilon_criterion(x=i[2], standard=j, epsilon=epsilon)):
                     '''
@@ -62,10 +63,10 @@ class vacancyMigration1(object):
                     '''
                     '''
                     print("编号为 {0} 的原子属于 {1} 层".format(
-                        self.site_positions.index(i), self.z_lst.index(j)))
+                        self.sites_position.index(i), self.z_lst.index(j)))
                     '''
-                    self.siteLabel_per_layer[self.z_lst.index(j)].append(
-                        self.site_positions.index(i))
+                    self.sitesLabel_per_layer[self.z_lst.index(j)].append(
+                        self.sites_position.index(i))
 
     # 计算层间距离
     def distances_bet_plane(self):
@@ -75,11 +76,11 @@ class vacancyMigration1(object):
             for j in range(len(self.z_lst)):
                 if (i < j):
                     minus_vector = vector_minus(
-                        self.site_positions[self.siteLabel_per_layer[i][0]], self.site_positions[self.siteLabel_per_layer[j][0]])
+                        self.sites_position[self.sitesLabel_per_layer[i][0]], self.sites_position[self.sitesLabel_per_layer[j][0]])
                     distance = inner_product(minus_vector, [0, 0, 1])
                     #distance_bet_planes.append(abs(distance))
-                    print("第 {0} 层 和 第 {1} 层的层间距离为 {2}".format(
-                        i, j, abs(distance)))
+                    # print("第 {0} 层 和 第 {1} 层的层间距离为 {2}".format(
+                        # i, j, abs(distance)))
 
     # 单层可视化
     def view_layer(self):
@@ -94,12 +95,12 @@ class vacancyMigration1(object):
     def get_diffusion_layers(self):
         self.get_layers()
 
-        self.layer3 = self.siteLabel_per_layer[3]     # layer: 16个原子
-        self.layer4 = self.siteLabel_per_layer[4]     # layer: 15个原子
-        print("编号为3的层内含有原子: ", self.layer3,
-              "\n总计{0}个".format(len(self.layer3)))
-        print("编号为4的层内含有原子: ", self.layer4,
-              "\n总计{0}个".format(len(self.layer4)))
+        self.layer3 = self.sitesLabel_per_layer[3]     # layer: 16个原子
+        self.layer4 = self.sitesLabel_per_layer[4]     # layer: 15个原子
+        # print("编号为3的层内含有原子: ", self.layer3,
+              # "\n总计{0}个".format(len(self.layer3)))
+        # print("编号为4的层内含有原子: ", self.layer4,
+              # "\n总计{0}个".format(len(self.layer4)))
 
     # (2) 找出同层内的最邻近原子（最近的2/4/6个原子)
     # center为中心原子
@@ -108,7 +109,7 @@ class vacancyMigration1(object):
         self.get_diffusion_layers()
         #print('\n')
 
-        # (2.1) 在layer3中，以某一原子找出相邻原子的大致距离
+        # (2.1) 在layer3/layer4中，以某一原子找出相邻原子的大致距离
         sites_distance_sameplane = []
         if self.center in self.layer3:
             for i in self.layer3:
@@ -119,7 +120,7 @@ class vacancyMigration1(object):
                 sites_distance_sameplane.append(self.distances_bet_sites[self.center][i])
         # 去除 1 ，因为np.sort(self.sites_distance_sameplane)[0] = 0
         self.nearest_criterion_sameplane = np.sort(sites_distance_sameplane)[1]
-        print("层内相邻原子间距为：", sites_distance_sameplane)
+        # print("层内相邻原子间距为：", sites_distance_sameplane)
 
         # (2.2) 以相邻原子间距为判据，找出某原子的所有相邻原子
         self.nearest_sites_samelayer = []
@@ -132,8 +133,8 @@ class vacancyMigration1(object):
             for i in self.layer4:
                 if (epsilon_criterion(x=self.distances_bet_sites[self.center][i], standard=self.nearest_criterion_sameplane, epsilon=epsilon)):
                     self.nearest_sites_samelayer.append(i)
-        print("同层原子间距为：{}".format(self.nearest_criterion_sameplane))
-        print('以编号为{}原子为中心，同层内最邻近原子为: '.format(center), self.nearest_sites_samelayer)
+        #print("同层原子间距为：{}".format(self.nearest_criterion_sameplane))
+        # print('以编号为{}原子为中心，同层内最邻近原子为: '.format(center), self.nearest_sites_samelayer)
         return self.nearest_sites_samelayer
 
     # (3) 寻找异层内最近原子
@@ -166,8 +167,8 @@ class vacancyMigration1(object):
             for i in self.layer3:
                 if (epsilon_criterion(x=self.distances_bet_sites[self.center][i], standard=self.nearest_criterion_diffplane, epsilon=epsilon)):
                     self.nearest_sites_difflayer.append(i)
-        print("异层原子间距为：{}".format(self.nearest_criterion_diffplane))
-        print('以编号为{}原子为中心，异层间最邻近原子及异层为: '.format(self.center), self.nearest_sites_difflayer)
+        # print("异层原子间距为：{}".format(self.nearest_criterion_diffplane))
+        # print('以编号为{}原子为中心，异层间最邻近原子及异层为: '.format(self.center), self.nearest_sites_difflayer)
         return self.nearest_sites_difflayer
 
 
@@ -178,9 +179,11 @@ class vacancyMigration1(object):
 
         self.center_lst = [self.center]
         for i in range(nsteps):
-            print("\n\n\n第{0}步扩散具体情况：".format(i+1))
+            #print("\n\n\n第{0}步扩散具体情况：".format(i+1))
             # prob_sites 是可能的扩散位点
             prob_site = self.get_nearest_sites_sameplane(center=self.center) + self.get_nearest_sites_diffplane(center=self.center)
+            # self.center_lst：单次扩散的空位位置
+            # self.centers_lst：不同次扩散的空位位置
             self.center = random.choice(prob_site)
             self.center_lst.append(self.center)
         
@@ -190,8 +193,26 @@ class vacancyMigration1(object):
         print('\n')
         return self.center
 
+# 四、连续扩散count次nsteps步
+    def n_migrations(self, center=57, nsteps=5, count=10):
+        # self.center_lst：单次扩散的空位位置
+        # self.centers_lst：不同次扩散的空位位置
+        self.centers_lst = []
+        self.count = count
+
+        for i in range(self.count):
+            center = self.migration_nsteps(center=self.center, nsteps=self.nsteps)
+            self.centers_lst.append(center)
+        with open('/Users/mac/Desktop/空位扩散/vanMig输出/positionOfVacancy', 'w') as f:
+            for i in self.centers_lst:
+                f.write(str(i) + '\n')
+        return self.centers_lst
+
+
+
 
 if __name__ == '__main__':
-    motion = vacancyMigration1(cifFile='/Users/mac/desktop/CONTCAR.cif')
+    motion = vacancyMigration1(cifFile='/Users/mac/Desktop/空位扩散/CONTCAR.cif')
     # motion.get_nearest_sites_sameplane(center=57)
-    motion.migration_nsteps(center=57, nsteps=5)
+    print("空位最后的位置是：", motion.migration_nsteps(center=57, nsteps=5))
+    print("10次扩散5步生成10种结构，空位的位置分别为：\n", motion.n_migrations(center=57, nsteps=5, count=10))
